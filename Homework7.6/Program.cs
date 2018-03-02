4a31e32d0b7affdbc97a2f1d81ec44700f235561
mike19952006 <mike19952006@gmail.com>
2018-03-03 00:47:31 +0200

    Заполнить двумерный массив числом, равным сумме ИНДЕКСОВ столбцов и вывести его в форме матрици на экран. Число строк и столбцов ввести с клавиатуры.

--- Homework7.6/Program.cs
+++ Homework7.6/Program.cs
@@ -10,6 +10,27 @@ namespace Homework7._6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Number of lines");
+            int lines = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number of columns");
+            int columns = int.Parse(Console.ReadLine());
+            int[,] array = new int[lines,columns];
+
+
+            int sum = 0;
+            for (int i = 0; i < columns; i++)
+                sum = sum + i;
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = sum;
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
